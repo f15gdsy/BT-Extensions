0c925a8b5b08ab1d09814f0a503599f93e857771
TheGameFather <f15gdsy@gmail.com>
2015-02-11 11:03:25 +0800

    - Init

---
+++ Platformer/Movement/MoveMotor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BT.Ex.Platformer {
+
+	public class MoveMotor : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+
+}
